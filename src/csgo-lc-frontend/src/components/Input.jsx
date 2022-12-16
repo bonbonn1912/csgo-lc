8f9092bc12e3e858d47ff5f11164bc5f762e7086
Tom Nüske <45395384+tomnueske@users.noreply.github.com>
2022-12-16 21:55:25 +0000

    added background text for textarea

--- src/csgo-lc-frontend/src/components/Input.jsx
+++ src/csgo-lc-frontend/src/components/Input.jsx
@@ -28,7 +28,7 @@ export const Input = (props) =>{
     <div className="md:flex md:items-top mb-6 flex-wrap justify-center">
       <div className="md:w-2/3 flex flex-wrap justify-center">
         <div className="flex justify-center font-serif m-4 text-gray-200 text-7xl">CSGO-LC</div>
-        <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 text-center" id="inline-full-name" type="textarea" defaultValue={"Your Status Message..."} onChange={handleChange}/>
+        <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 text-center" id="inline-full-name" type="textarea" defaultValue={""} placeholder="Your Status Message..." onChange={handleChange}/>
         <button disabled={buttonIsDisabled} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow m-5" type="submit" onClick={() =>props.handleClick(statusMessage)}>{props.message}</button>
       </div>
     </div>
