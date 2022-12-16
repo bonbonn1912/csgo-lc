9d2e370f806635993d50d4ff54645e5dd551af42
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-12-16 23:25:34 +0100

    Update post.js

--- src/backend/server/routes/post.js
+++ src/backend/server/routes/post.js
@@ -8,6 +8,18 @@ POST_ROUTES.post('/api/v1/status', async (req, res) =>{
     res.send({players:players})
 })
 
+<<<<<<< Updated upstream
+=======
+POST_ROUTES.post('/api/v1/consoleelo', async (req, res) =>{
+    console.log(req.body.player)
+    let players = await AddRanksToPlayer(req.body.player)
+    let key = getHash(JSON.stringify(players))
+    GAME_STORE.set(key, players)
+    res.send({url: `https://csgo-lc-production.up.railway.app/room?key=${key}` })
+})
+
+
+>>>>>>> Stashed changes
 
 
 module.exports = POST_ROUTES
