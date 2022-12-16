581afd1c065fc2e0f15ae3dd6745f5512421b889
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-12-16 23:15:05 +0100

    added room support

--- src/backend/server/routes/post.js
+++ src/backend/server/routes/post.js
@@ -1,12 +1,25 @@
 const POST_ROUTES = require('express').Router()
 const AddRanksToPlayer = require('../../stats/addStats.js')
+let GAME_STORE = require('../store')
+const getHash = require('../../stats/hash.js')
 
 
 
 POST_ROUTES.post('/api/v1/status', async (req, res) =>{
     let players = await AddRanksToPlayer(req.body.player)
+    let key = getHash(JSON.stringify(players))
+    console.log(key)
+    GAME_STORE.set(key, players)
     res.send({players:players})
 })
+
+POST_ROUTES.post('/api/v1/consoleelo', async (req, res) =>{
+    let players = await AddRanksToPlayer(req.body.player)
+    let key = getHash(JSON.stringify(players))
+    GAME_STORE.set(key, players)
+    res.send({url: `https://csgo-lc-production.up.railway.app/room?key=${key}` })
+})
+
 
 
 
